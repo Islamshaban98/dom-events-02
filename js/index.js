3520577469d6a6ae59d0e0aad45afb2cd98c5bea
Islamshaban98 <islamshaban998@gmail.com>
2020-09-30 14:17:09 +0300

    editing on code

--- js/index.js
+++ js/index.js
@@ -1,22 +1,33 @@
 // Here you can access the colors variable
 // have fun
-const button =document.getElementsByTagName('button');
-button[0].addEventListener('click', addColor);
+const button = document.getElementsByTagName("button");
+button[0].addEventListener("click", addColor);
 
-function addColor(){
-    const input = document.getElementsByTagName('input');
-    const random = Math.floor(Math.random() * colors.length);
-    const coloredDiv = document.getElementsByClassName('pallete')[0];
-    coloredDiv.style.background = '#'+random;
-    input[0].value = '#'+random;
+function addColor() {
+  const input = document.getElementsByTagName("input");
+  const random = colors[Math.floor(Math.random() * colors.length)];
+  const coloredDiv = document.getElementsByClassName("pallete")[0];
+  coloredDiv.style.background = random;
+  input[0].value = random;
+  input[0].style.color = random;
+  const h1 = document.getElementsByTagName("h1");
+  h1[0].style.background = random;
+  button[0].style.background = random;
+  const text = document.getElementsByClassName("color-input");
+  text[0].style.color = random;
 }
 
-const input = document.getElementsByTagName('input');
-input[0].addEventListener('change',typeColor);
+const input = document.getElementsByTagName("input");
+input[0].addEventListener("change", typeColor);
 
-function typeColor(){
-const colorValue = input[0].value;
-const coloredDiv = document.getElementsByClassName('pallete')[0];
-coloredDiv.style.background = colorValue;
-
+function typeColor() {
+  const colorValue = input[0].value;
+  const coloredDiv = document.getElementsByClassName("pallete")[0];
+  coloredDiv.style.background = colorValue;
+  input[0].style.color = colorValue;
+  const h1 = document.getElementsByTagName("h1");
+  h1[0].style.background = colorValue;
+  button[0].style.background = colorValue;
+  const text = document.getElementsByClassName("color-input");
+  text[0].style.color = colorValue;
 }
