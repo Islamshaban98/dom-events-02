3b2a765859fb009c794812ffa96a14eaae4a66e5
Islamshaban98 <islamshaban998@gmail.com>
2020-10-01 17:28:47 +0300

    using querey selector insted of getElement

--- js/index.js
+++ js/index.js
@@ -1,33 +1,33 @@
 // Here you can access the colors variable
 // have fun
-const button = document.getElementsByTagName("button");
-button[0].addEventListener("click", addColor);
+const button = document.querySelector("button");
+button.addEventListener("click", addColor);
 
 function addColor() {
-  const input = document.getElementsByTagName("input");
+  const input = document.querySelector("input");
   const random = colors[Math.floor(Math.random() * colors.length)];
-  const coloredDiv = document.getElementsByClassName("pallete")[0];
+  const coloredDiv = document.querySelector("#pallete");
   coloredDiv.style.background = random;
-  input[0].value = random;
-  input[0].style.color = random;
-  const h1 = document.getElementsByTagName("h1");
-  h1[0].style.background = random;
-  button[0].style.background = random;
-  const text = document.getElementsByClassName("color-input");
-  text[0].style.color = random;
+  input.value = random;
+  input.style.color = random;
+  const h1 = document.querySelector("h1");
+  h1.style.background = random;
+  button.style.background = random;
+  const text = document.querySelector("label");
+  text.style.color = random;
 }
 
-const input = document.getElementsByTagName("input");
-input[0].addEventListener("change", typeColor);
+const input = document.querySelector("input");
+input.addEventListener("change", typeColor);
 
-function typeColor() {
-  const colorValue = input[0].value;
-  const coloredDiv = document.getElementsByClassName("pallete")[0];
-  coloredDiv.style.background = colorValue;
-  input[0].style.color = colorValue;
-  const h1 = document.getElementsByTagName("h1");
-  h1[0].style.background = colorValue;
-  button[0].style.background = colorValue;
-  const text = document.getElementsByClassName("color-input");
-  text[0].style.color = colorValue;
+function typeColor(event) {
+  const {value} = event.target;
+  const coloredDiv = document.querySelector("#pallete");
+  coloredDiv.style.background = value;
+  input.style.color = value;
+  const h1 = document.querySelector("h1");
+  h1.style.background = value;
+  button.style.background = value;
+  const text = document.querySelector("label");
+  text.style.color = value;
 }
